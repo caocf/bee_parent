181c5112eaab09c5094c4a793ab8f1db4f1c6725
suntongwei <alpha27@126.com>
2015-05-04 18:18:04 +0800

    add shop stat url

---
+++ bee_web/src/main/java/com/bee/services/stat/impl/StatShopService.java
@@ -0,0 +1,11 @@
+package com.bee.services.stat.impl;
+
+import com.bee.services.stat.IStatShopService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by suntongwei on 15/5/4.
+ */
+@Service
+public class StatShopService implements IStatShopService {
+}
