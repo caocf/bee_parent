6f272a6f8e7466efa9f4302553405b2b93582a7f
suntongwei <alpha27@126.com>
2015-10-04 17:29:15 +0800

    update huanxin app_client

--- bee_web/src/main/java/com/easemob/server/comm/Constants.java
+++ bee_web/src/main/java/com/easemob/server/comm/Constants.java
@@ -13,11 +13,11 @@ public interface Constants {
 	// API_SERVER_HOST
 	public static String API_SERVER_HOST = "a1.easemob.com";
 	// APPKEY
-	public static String APPKEY = "qsd#beetest";
+	public static String APPKEY = "bee#bee";
 	// APP_CLIENT_ID
-	public static String APP_CLIENT_ID = "YXA6L41z0OXdEeS7Yg3JmtiMIA";
+	public static String APP_CLIENT_ID = "YXA6p8vbAETBEeWINX_9ozBqKQ";
 	// APP_CLIENT_SECRET
-	public static String APP_CLIENT_SECRET = "YXA6QYbEjnfuDpHKyHsemkGdEWiM15Y";
+	public static String APP_CLIENT_SECRET = "YXA6GFlNS8EhirYPeRgLEbOS9k_LJhw";
     // ORG_ADMIN_USERNAME
     public static String ORG_ADMIN_USERNAME = "";
     // ORG_ADMIN_PASSWORD
